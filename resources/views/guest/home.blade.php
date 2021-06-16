89a63194b1d23f1ce5a26827d0a72fcbde8d3d7c
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-06-16 21:03:25 +0200

    prepare home for vue

---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Boolpress</title>
+
+    </head>
+    <body>
+        <div>
+            <h1>WORK IN PROGRESS</h1>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.welcome');
-});
-
+// BACKEND
 // Routes for authentication
 Auth::routes();
 
@@ -31,3 +28,8 @@ Route::prefix('admin')
         // Route fore CRUD
         Route::resource('/posts', 'PostController');
     });
+
+// FRONTEND
+Route::get('{any?}', function () {
+    return view('guest.home');
+})->where('any', '.*');
