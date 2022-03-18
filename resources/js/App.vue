743fbcce0488bbfc81417f54b677987cafa08dc6
Andrea-Mattia <andrea.mattia94@gmail.com>
2022-03-18 20:02:49 +0100

    add: api route, axios call in vue and post printed

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -3,28 +3,69 @@
         <Header />
 
         <main>
-            <h1>WORK IN FROGRESS</h1>
+            <div class="container">
+                <h1>Blog</h1>
+
+                <!-- Print posts -->
+                <article v-for="post in posts" :key="post.id">
+                    <h2>{{ post.title }}</h2>
+                    <div class="excerpt">
+                        Excerpt goes here
+                    </div>
+                    <div>{{ post.created_at }}</div>
+                    <!-- Go to detail page -->
+                    <a href="">Read more</a>
+                </article>
+            </div>
         </main>
 
-        <footer>
-            FOOTER HERE
-        </footer>
+        <Footer />
     </div>
 </template>
 
 <script>
+import axios from "axios";
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
     name: "App",
     components: {
-        Header
+        Header,
+        Footer
+    },
+    data() {
+        return {
+            api: "http://127.0.0.1:8000/api/posts",
+            posts: []
+        };
+    },
+    created() {
+        this.getPosts();
+    },
+    methods: {
+        getPosts() {
+            axios
+                .get(this.api)
+                .then(res => {
+                    this.posts = res.data;
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
     }
 };
 </script>
 
 <style lang="scss">
+@import "../sass/frontoffice/_utilities.scss";
 body {
     font-family: sans-serif;
+    article {
+        .excerpt {
+            margin: 1rem auto;
+        }
+    }
 }
 </style>
---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer>
+        <div class="container">
+            FOOTER HERE
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+};
+</script>
+
+<style lang="scss" scoped>
+footer {
+    height: 50px;
+    margin-top: 3rem;
+    text-align: center;
+    background: lightblue;
+}
+</style>
